2084a57e1fe3ff8c369499a5febd6221342c17a6
mikael <mikael.stray.froyshov@avinor.no>
2025-07-04 08:33:31 +0200

    feat: add test page for ws

--- src/features/location/pages/LocationPage.tsx
+++ src/features/location/pages/LocationPage.tsx
@@ -1,12 +1,16 @@
-import {useGetLiteLocationByIdQuery} from "../locationApi.ts";
+import {useGetLiteLocationByIdQuery, useGetOccupancyStatusQuery} from "../locationApi.ts";
+import {UpdatePax} from "../test/UpdatePax.tsx";
 
 export const LocationPage = () => {
     const { data: location } = useGetLiteLocationByIdQuery(2)
+    const { data: livePax } = useGetOccupancyStatusQuery(2)
 
     console.log(location)
     return (
         <>
             <p>{location?.name}</p>
+            <p>{livePax?.pax}</p>
+            <UpdatePax />
         </>
     )
 }
