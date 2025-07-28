098c4d59ed5ab0eaed9a45fc556ee52e1aa03484
Inana <inana.Baker@avinor.no>
2025-07-28 12:36:39 +0200

    add root.tsx with logic for conditional subheader

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,19 @@
-import {createRootRoute, Link, Outlet} from "@tanstack/react-router";
-import {TanStackRouterDevtools} from "@tanstack/react-router-devtools";
-import {Header} from "../style/header.tsx";
+import { createRootRoute, Outlet, useLocation, useNavigate } from "@tanstack/react-router";
+import { Header } from "../style/header";
 
 export const Route = createRootRoute({
-    component: () => (
-        <>
-            <Header/>
-            <hr />
-            <Outlet />
-            <TanStackRouterDevtools />
-        </>
-    ),
-})
+  component: () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const isHome = location.pathname === "/";
+    const showBackButton = !isHome;
+
+    return (
+      <>
+        <Header showBackButton={showBackButton} onBack={() => navigate(-1)} />
+        <Outlet />
+      </>
+    );
+  },
+});
