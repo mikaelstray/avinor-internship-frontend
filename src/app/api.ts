d872d106a36ff2f0577463f27019a6617892c459
mikael <mikael.stray.froyshov@avinor.no>
2025-08-05 13:04:48 +0200

    feat: add prepare header

--- src/app/api.ts
+++ src/app/api.ts
@@ -2,7 +2,13 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: '/api',
+        prepareHeaders: (headers) => {
+            headers.set('Accept', 'application/json');
+            return headers;
+        }
+    }),
     tagTypes: [],
     endpoints: () => ({})
 })
